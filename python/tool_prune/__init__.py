b5b52985a8d89077ef1f37689ca36e48b6d1f7da
hemanth <hemanth.hm@gmail.com>
2026-09-17 09:40:49 -0700

    chore: bump python version to 0.1.1 for PyPI documentation update

--- python/tool_prune/__init__.py
+++ python/tool_prune/__init__.py
@@ -1,5 +1,5 @@
 from .router import ToolPrune, CandidateTool, SelectionResult, prune
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["ToolPrune", "CandidateTool", "SelectionResult", "prune"]
 
